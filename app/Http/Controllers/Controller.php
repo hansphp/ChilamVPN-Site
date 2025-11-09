f915875fdc06a73a02d9166a2b037fd4fdb6f51f
Hans Herrera <hans.php@gmail.com>
2025-11-09 00:27:51 -0600

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,5 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    protected const CONTENT_SECURITY_POLICY = "default-src 'self'; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; script-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self' data:; base-uri 'self'; frame-ancestors 'none';";
+    protected const CONTENT_SECURITY_POLICY = "default-src 'self'; connect-src 'self' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; script-src 'self' 'unsafe-inline' https://static.cloudflareinsights.com; img-src 'self' data:; font-src 'self' data:; base-uri 'self'; frame-ancestors 'none';";
 }
